12f7b3e54ece906639a6630608504002ba7894f6
KKonsul <KKonsul@alfabank.ru>
2025-01-10 10:16:26 +0300

    express init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log('run');
+import express from 'express';
+
+const expressApp = express();
+
+expressApp.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+expressApp.get('/ideas', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: 'Fuck',
+    },
+  ]);
+});
+
+const PORT = 3000;
+
+expressApp.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
