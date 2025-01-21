d6f671564c51c2b0f0f37cfc6415d516247982e0
KKonsul <KKonsul@alfabank.ru>
2025-01-21 19:12:54 +0300

    Add Segment component

--- webapp/src/pages/ViewideaPage/index.tsx
+++ webapp/src/pages/ViewideaPage/index.tsx
@@ -1,6 +1,6 @@
+import { Segment } from '../../components/Segment';
 import { ViewIdeaRouteParams } from '../../lib/routes';
 import css from './index.module.scss';
-
 import { trpc } from '../../lib/trpc';
 import { useParams } from 'react-router-dom';
 
@@ -22,10 +22,8 @@ export const ViewIdeaPage = () => {
   }
 
   return (
-    <div>
-      <h1 className={css.title}>{data.idea.name}</h1>
-      <p className={css.description}>{data.idea.description}</p>
+    <Segment title={data.idea.name} description={data.idea.description}>
       <div className={css.text} dangerouslySetInnerHTML={{ __html: data.idea.text }} />
-    </div>
+    </Segment>
   );
 };
