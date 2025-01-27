dd78448ea44b759106cb7abe7ff8e8f5f35a3fe6
KKonsul <KKonsul@alfabank.ru>
2025-01-27 20:22:32 +0300

    Add success message

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -3,10 +3,12 @@ import { Segment } from '../../components/Segment';
 import { Textarea } from '../../components/Textarea';
 import { trpc } from '../../lib/trpc';
 import { useFormik } from 'formik';
+import { useState } from 'react';
 import { withZodSchema } from 'formik-validator-zod';
 import { zCreateIdeaTrpcInput } from '@ideanick/backend/src/router/createIdea/input';
 
 export const NewIdeaPage = () => {
+  const [successMessageVisible, setSuccessMessageVisible] = useState(false);
   const createIdea = trpc.createIdea.useMutation();
 
   const formik = useFormik({
@@ -19,6 +21,13 @@ export const NewIdeaPage = () => {
     validate: withZodSchema(zCreateIdeaTrpcInput),
     onSubmit: async (values) => {
       await createIdea.mutateAsync(values);
+
+      formik.resetForm();
+      setSuccessMessageVisible(true);
+
+      setTimeout(() => {
+        setSuccessMessageVisible(false);
+      }, 3000);
     },
   });
 
@@ -34,7 +43,10 @@ export const NewIdeaPage = () => {
         <Input name="nick" label="Nick" formik={formik} />
         <Input name="description" label="Description" formik={formik} />
         <Textarea name="text" label="Text" formik={formik} />
+
         {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
+        {successMessageVisible && <div style={{ color: 'green' }}>Idea created!</div>}
+
         <button type="submit" disabled={formik.isSubmitting}>
           {formik.isSubmitting ? 'Submitting...' : 'Create Idea'}
         </button>
