1ef4158ee8535dda701efc5518b9a51f547d59ed
KKonsul <KKonsul@alfabank.ru>
2025-01-28 20:04:00 +0300

    Add error handling for idea create

--- backend/src/router/createIdea/index.ts
+++ backend/src/router/createIdea/index.ts
@@ -3,6 +3,12 @@ import { ideas } from '../../lib/ideas';
 import { zCreateIdeaTrpcInput } from './input';
 
 export const createIdeaTrpcRoute = trpc.procedure.input(zCreateIdeaTrpcInput).mutation(({ input }) => {
+  const idea = ideas.find((idea) => idea.nick === input.nick);
+
+  if (idea) {
+    throw Error(`Idea with nick '${input.nick}' already exists`);
+  }
+
   ideas.unshift(input);
 
   return true;
