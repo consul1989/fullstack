4cff9fdd106b56fcd5085805cbc8f3b9350acb52
KKonsul <KKonsul@alfabank.ru>
2025-01-30 21:39:34 +0300

    Add endpoint for signUp

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -2,11 +2,13 @@ import { trpc } from '../lib/trpc';
 import { getIdeasTrpcRoute } from './getIdeas';
 import { getIdeaTrpcRoute } from './getIdea';
 import { createIdeaTrpcRoute } from './createIdea';
+import { signUpTrpcRoute } from './signUp';
 
 export const trpcRouter = trpc.router({
   getIdeas: getIdeasTrpcRoute,
   getIdea: getIdeaTrpcRoute,
   createIdea: createIdeaTrpcRoute,
+  signUp: signUpTrpcRoute,
 });
 
 export type TrpcRouter = typeof trpcRouter;
