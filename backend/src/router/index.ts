d0727c11918cc0ac565dd16588f0647f05dab8e4
KKonsul <KKonsul@alfabank.ru>
2025-02-03 22:13:18 +0300

    add signIn route

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -2,12 +2,14 @@ import { trpc } from '../lib/trpc';
 import { getIdeasTrpcRoute } from './getIdeas';
 import { getIdeaTrpcRoute } from './getIdea';
 import { createIdeaTrpcRoute } from './createIdea';
+import { signInTrpcRoute } from './signIn';
 import { signUpTrpcRoute } from './signUp';
 
 export const trpcRouter = trpc.router({
   getIdeas: getIdeasTrpcRoute,
   getIdea: getIdeaTrpcRoute,
   createIdea: createIdeaTrpcRoute,
+  signIn: signInTrpcRoute,
   signUp: signUpTrpcRoute,
 });
 
